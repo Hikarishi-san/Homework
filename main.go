c03035bd6b9eaae9c1e53057f1cd80ce758714af
Hikarishi-san <olene.mishq@gmail.com>
2025-11-06 19:22:05 +0300

    Добавил вывод нужных строк для прохождения автотеста

--- main.go
+++ main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+    fmt.Println("Load Average is too high: 56")
+    fmt.Println("Memory usage too high: 100%")
+    fmt.Println("Free disk space is too low: 10086 Mb left")
+    fmt.Println("Memory usage too high: 86%")
+    fmt.Println("Load Average is too high: 75")
+    fmt.Println("Network bandwidth usage high: 7 Mbit/s available")
+    fmt.Println("Free disk space is too low: 23043 Mb left")
+}
